d862c42cea97d6592c4040c83abf9b68e76a92e6
yilan <zhaoyinlan@live.cn>
2014-08-09 22:27:41 +0800

    Update ip.py
    add args format

--- ip.py
+++ ip.py
@@ -1,25 +1,41 @@
+# -*- coding: utf-8 -*-
+
 __author__ = 'fish'
 
-import urllib, urllib2
+import urllib
 import json
 import sys
 import re
 
 
 def get_info(ip):
-    urlfp = urllib.urlopen('http://ip.taobao.com/service/getIpInfo.php?ip=' + ip)
-    ipdata = urlfp.read()
-    urlfp.close()
-    allinfo = json.loads(ipdata)
-    if "data" not in allinfo:
+    url_fp = urllib.urlopen(
+        'http://ip.taobao.com/service/getIpInfo.php?ip=' + ip)
+    ip_data = url_fp.read()
+    url_fp.close()
+    all_info = json.loads(ip_data)
+    if "data" not in all_info:
         return None
-    data = allinfo["data"]
-    return '%s %s' %(data["region"],data["city"])
+    data = all_info["data"]
+    return '%s %s' % (data["region"], data["city"])
 
 
+'''
+    traceroute -n ip | awk '{print $2}'
+    input format :
+    ....
+    61.164.31.121
+    202.97.34.57
+    *
+    *
+    220.181.70.106
+    ip address
+'''
 if __name__ == "__main__":
-    if len(sys.argv)>=2:
+    if len(sys.argv) >= 2:
+        ips = filter(lambda x: x != "", sys.argv[1].split("\n"))
         pat = re.compile("^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$")
-        ip = sys.argv[1]
-        if pat.search(ip):
-            print get_info(ip)
+        for ip in ips:
+            ip = ip.strip()
+            if pat.search(ip):
+                print ip, get_info(ip)
